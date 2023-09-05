32bc594bde5cd3ab5114fc463c343be9609fd07a
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-05 07:01:59 +0000

    datatype and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1443
+let accountEmail = "khalid@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 //Not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "23321"
+accountCity = " Patna "
+
+console.log(accountId);
+console.table([accountId , accountEmail, accountPassword , accountCity , accountState]) 
+  
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"Use Strict"; // treat all JS as newer version
+
+// alert( 3+3) we are using nodejs, not browser
+
+
+console.log(3+3) ; console.log("khalid")  //Code readablility is also very important in programming*/
+
+let name = "Khalid"
+let age = 20
+
+
+
+// Data types
+// number => 2 to the power 53
+//bigInt => very big numbers
+// string => ""
+// boolean => true/false
+// null => Standalone value / Or object
+// undefined => Undefined
+// Symbols => Unique
+
+
+// objects
+
+console.log(typeof "khalid");
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
