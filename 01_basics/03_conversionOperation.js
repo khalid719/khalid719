bdc136a8016e1a318eb0a2090343e5e654cdc377
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-06 04:58:37 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,52 @@
 // Q1
 
-/*let score = undefined
+let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInboolean = Boolean(score)
 
-console.log(typeof valueInboolean);
-console.log(valueInboolean);*/
+// console.log(typeof valueInboolean);
+// console.log(valueInboolean);
 
-// Q2 :
+// // Q2 :
 
 let match = "INDVSPak"
 
-console.log(typeof match);
-console.log(typeof (match));
+// console.log(typeof match);
+// console.log(typeof (match));
 
-console.log(match);
+// console.log(match);
  
 let convertmatch = Number(match)
 
-console.log(typeof convertmatch);
+// console.log(typeof convertmatch);
 
-console.log(convertmatch);
+// console.log(convertmatch);
+
+
+// *********** operations *********//
+
+
+
+let value = 3
+let negvalue =-value
+// console.log(negvalue);
+
+//simple arithmetics
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 ="Hello"
+let str2 =" khalid"
+
+let str3 = str1 + str2
+
+console.log(str3);
