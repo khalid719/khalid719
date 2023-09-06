2e284a92fb2feb30b4af46f3856a164d6180afcf
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-06 04:33:58 +0000

    Converion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// Q1
+
+/*let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInboolean = Boolean(score)
+
+console.log(typeof valueInboolean);
+console.log(valueInboolean);*/
+
+// Q2 :
+
+let match = "INDVSPak"
+
+console.log(typeof match);
+console.log(typeof (match));
+
+console.log(match);
+ 
+let convertmatch = Number(match)
+
+console.log(typeof convertmatch);
+
+console.log(convertmatch);
