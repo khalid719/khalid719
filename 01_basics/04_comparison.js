116c6be8dc0b1fb37ca16987bf52eb4bec7555ae
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-06 07:46:35 +0000

    comparison of datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// comparison with same datatype is easy to understand.
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+// console.log(2!==1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// comparison with different datatype.
+
+// console.log("23" > 23);
+// console.log("23" >= 23);
+// console.log("23" <= 23);
+// console.log("23" < 23);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
