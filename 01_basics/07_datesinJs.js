79c3d358ea4319c49ec22099f08b1913ae0d4d68
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-20 03:55:22 +0000

    dates inJS

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString());
+
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2023,9,20)
+console.log(myCreatedDate.toDateString());
