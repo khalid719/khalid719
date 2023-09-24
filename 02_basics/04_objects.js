b69835d9d9927f5227413f5beff763ec75762197
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-24 05:10:16 +0000

    Objects and JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -49,4 +49,30 @@ const obj3 = {...obj1, ...obj2}
 // console.log(Object.keys(tinderuser));
 // console.log(Object.entries(tinderuser));
 
-console.log(tinderuser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderuser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const{courseInstructor: instructor} = course 
+const{coursename :name} = course 
+
+console.log(instructor);
+console.log(name);
+
+
+//JSON
+
+// {
+//     "name":"hitesh",
+//     "coursename":"JS in hindi",   // keys are also strings.
+//     "price":"free"
+// }
+
+/// API's are JSON format document.
