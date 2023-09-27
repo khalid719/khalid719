28dc062fa073765a6175c46e2160210fcf6d4e7a
khalid719 <142531366+khalid719@users.noreply.github.com>
2023-09-27 06:25:28 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,57 @@
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+   
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "khalid"
+
+    function two(){
+        const website = "YouTube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+
+if (true) {
+   const username = "khalid"
+   const id = 234
+   
+   if (true) {
+    const website = "YouTube"
+    // console.log(username);
+    
+    //    console.log(website);
+   }
+
+// console.log(id);
+}   
+
+  
+console.log(addOne(8))
+
+function addOne(num){
+    // console.log(num + 1);
+    return num + 1
+}
+
+console.log(addTwo(5))
+
+const addTwo = function(num){
+    return num + 2
+}
